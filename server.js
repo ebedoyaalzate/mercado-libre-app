452c97d7752abba418506b844cfed12930995a71
ebedoyaalzate <esteban.bedoyaa@udea.edu.co>
2019-08-10 11:07:19 -0500

    change start path

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/MERCADO-LIBRE-APP'));
+app.use(express.static(__dirname + '/dist/mercado-libre-app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MERCADO-LIBRE-APP/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mercado-libre-app/index.html'));
 });
 
 // default Heroku port
